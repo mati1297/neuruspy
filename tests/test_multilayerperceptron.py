3bdc4f1d745d39ab7f6628b727fe3e686d0c07d9
Matias Charrut <mcharrut@fi.uba.ar>
2022-07-11 12:59:48 -0300

    Documented activation and perceptron tests

--- tests/test_multilayerperceptron.py
+++ tests/test_multilayerperceptron.py
@@ -6,6 +6,8 @@ from neuruspy.activation import Relu
 
 class TestLayer(unittest.TestCase):
     def test_add_bias(self):
+        """Tests if bias is added correctly.
+        """
         data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         data_bias = np.array([[1, 1, 2, 3], [1, 4, 5, 6], [1, 7, 8, 9]])
 
@@ -14,6 +16,8 @@ class TestLayer(unittest.TestCase):
         nptest.assert_equal(layer._add_bias(data), data_bias)
 
     def test_add_bias_does_nothing(self):
+        """Tests if bias is not added when input data already has it.
+        """
         data = np.array([[1, 1, 2, 3], [1, 4, 5, 6], [1, 7, 8, 9]])
         data_bias = np.array([[1, 1, 2, 3], [1, 4, 5, 6], [1, 7, 8, 9]])
 
@@ -21,19 +25,9 @@ class TestLayer(unittest.TestCase):
 
         nptest.assert_equal(layer._add_bias(data), data_bias)
 
-    def test_relu_forward(self):
-        data = np.array([-5, 2, 3, -8, 0, 4])
-        output = np.array([0, 2, 3, 0, 0, 4])
-
-        nptest.assert_equal(Relu.forward(data), output)
-
-    def test_relu_back(self):
-        data = np.array([-5, 2, 3, -8, 0, 4])
-        output = np.array([0, 1, 1, 0, 0, 1])
-
-        nptest.assert_equal(Relu.back(data), output)
-
     def test_predict_h(self):
+        """Tests if predict without activation function works ok.
+        """
         data = np.array([[1, 2, 3], [4, 5, 6], [7, -8, -9]])
         w = np.ones((1, 4))
 
@@ -42,6 +36,8 @@ class TestLayer(unittest.TestCase):
         nptest.assert_equal(layer.predict_h(data), np.array([[7], [16], [-9]]))
 
     def test_predict(self):
+        """Tests if predict works ok.
+        """
         data = np.array([[1, 2, 3], [4, 5, 6], [7, -8, -9]])
         w = np.ones((1, 4))
 
@@ -50,6 +46,12 @@ class TestLayer(unittest.TestCase):
         nptest.assert_equal(layer.predict(data), np.array([[7], [16], [0]]))
 
     def test_backpropagate_last_layer(self):
+        """Tests if backpropagation works ok.
+
+        Tests if backpropagation works ok by backpropagating with a layer
+        as if it was the last of a network, so it receives as delta the
+        difference between output and desired output.
+        """
         data = np.array([[1, 2, 3], [7, -8, -9]])
         w = np.ones((1, 4))
         desired = np.array([[2], [4]])
@@ -67,12 +69,19 @@ class TestLayer(unittest.TestCase):
         nptest.assert_equal(prev_delta, expected_prev_delta)
     
     def test_w_not_matching_dimensions(self):
+        """Tests if creating a layer with wrong w raises exception.
+        
+        Tests if creating a layer with incorrect weight matrix dimensions
+        raises an ValueError exception.
+        """
         w = np.ones((1, 3))
 
         self.assertRaises(ValueError, Layer, 3, 1, Relu, w)
 
 class TestMultilayerPerceptron(unittest.TestCase):
     def test_predict_internal(self):
+        """Tests if predict internal works ok.
+        """
         data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         w1 = np.ones((2, 4))
         w2 = np.ones((1, 3)) * 2
@@ -87,6 +96,8 @@ class TestMultilayerPerceptron(unittest.TestCase):
         nptest.assert_equal(output, perceptron.predict_internal(data))
 
     def test_predict(self):
+        """Tests if predict works ok.
+        """
         data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         w1 = np.ones((2, 4))
         w2 = np.ones((1, 3)) * 2
@@ -100,6 +111,8 @@ class TestMultilayerPerceptron(unittest.TestCase):
         nptest.assert_equal(output, perceptron.predict(data))
 
     def test_evaluate(self):
+        """Tests if evaluate works ok.
+        """
         data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         w1 = np.ones((2, 4))
         w2 = np.ones((1, 3)) * 2
@@ -115,12 +128,19 @@ class TestMultilayerPerceptron(unittest.TestCase):
         self.assertEqual(output, perceptron.evaluate(data, desired))
 
     def test_adding_a_not_matching_layer(self):
+        """Tests if adding a not matching layer raises exception.
+        
+        Tests if adding a layer which does not match its inputs with last
+        layer outputs raises a ValueError exception.
+        """
         perceptron = MultilayerPerceptron()
         perceptron.add_layer(Layer(1, 2, Relu))
 
         self.assertRaises(ValueError, perceptron.add_layer, Layer(3, 1, Relu))
 
     def test_train_two_layers(self):
+        """Tests if training a two layers network works ok.
+        """
         perceptron = MultilayerPerceptron()
         w1 = np.ones((1,2))
         w2 = np.ones((1,2)) * 2
